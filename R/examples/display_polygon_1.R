94c80378c7ffa7f10d72c8fac75d7f822282be34
Michael Harper <mikey-harper@hotmail.com>
2018-10-12 14:22:52 +0100

    Add examples for R functions

---
+++ R/examples/display_polygon_1.R
@@ -0,0 +1,6 @@
+# Find file path to example plot
+poly_path <- system.file("extdata/polygons/001_js_polygon.shp",
+                         package = "polyCheck")
+
+# Display the output Map
+display_polygon(poly_path)
